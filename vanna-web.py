1eae7ffc39ec8a4d60db7a932d58bc6f195ae0c0
nickyin <nickyin@tw.ibm.com>
2025-02-10 12:05:28 +0800

    a

--- vanna-web.py
+++ vanna-web.py
@@ -1,6 +1,9 @@
 from vanna.flask import VannaFlaskApp
 from vanna.ollama import Ollama
 from vanna.chromadb import ChromaDB_VectorStore
+import psycopg2
+import pandas as pd
+from typing import Dict, Optional
 
 class MyVanna(ChromaDB_VectorStore, Ollama):
     def __init__(self, config=None):
@@ -13,6 +16,24 @@ vn = MyVanna(config={
     'ollama_host': "http://ollama.webtw.xyz:11434",
     'path': '/data/chromadb'
 })
+
+def run_sql(sql: str) -> pd.DataFrame:
+    with psycopg2.connect(
+        host='10.10.10.168',
+        database='postgres',
+        user='admin',
+        password='admin',
+        port='5432'
+    ) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            columns = [desc[0] for desc in cursor.description]
+            df = pd.DataFrame(result, columns=columns)
+            return df
+
+vn.run_sql = run_sql
+vn.run_sql_is_set = True
 
 app = VannaFlaskApp(vn)
 app.run()
